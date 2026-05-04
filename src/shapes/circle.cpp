0cd7f19d16babc60519139ee5414e4135373e42b
Rbapt <rbapt360@gmail.com>
2026-05-04 15:11:58 +0200

    ajout aire cercle

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -7,3 +7,8 @@ Circle::Circle(double r, Point c) : radius(r), center(c) {};
 double Circle::circumference(){
     return M_PI * radius * 2;
 }
+
+double Circle::area(){
+    return M_PI * radius * radius;
+}
+
