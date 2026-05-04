6ed5b1cc775fe38b8dbbb73f9a262fe62c53969d
Rbapt <rbapt360@gmail.com>
2026-05-04 15:07:53 +0200

    ajout perimetre du cercle

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -1,4 +1,9 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
+#include  <cmath>
 
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
+double Circle::circumference(){
+    return M_PI * radius * 2;
+}
