22b74d4e8373b1815fceff25def2287bed8a7cbc
Matisse Guibert <matisse.guibert@universite-paris-saclay.fr>
2026-05-04 15:35:35 +0200

    Implémentation de la fonction side de la structure square

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,29 @@
+#include "draw.hpp"
 #include "point.hpp"
+#include "shapes.hpp"
 #include "shapes/square.hpp"
+#include <vector>
 
-Square::Square(Point P, Point R) : A(P), C(R) {};
+Square::Square(Point P, Point Q) : A(P),C(Q){}
+
+void Square::draw() {
+	
+	// Define points in a vector
+	std::vector<Point> points = {
+		A,
+		Point(C.x, A.y),
+		C,
+		Point(A.x, C.y),
+		A
+		
+	};
+	
+	// Draw the picture by connecting the points
+	draw_picture(points);
+	
+}
+double Square::side(){
+	double s = C.x - A.x;
+	if(s<0){s=-s;};
+	return s; 
+}
